6348be5193196d579906a70dd95e353fb9134e1e
juampam <jpablomuralles@yandex.com>
2022-08-18 17:36:47 -0600

    Default commit while developing

--- start.sh
+++ start.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-rm log.exp
 user=$(sed -n 1p ~/.gituserdata)
 pass=$(sed -n 2p ~/.gituserdata)
-echo "#!/usr/bin/expect" >> log.exp
-echo spawn ./autocom.sh >> log.exp
-printf "expect  \"Username for 'https://github.com':\" {\n" >> log.exp
-echo send "$user\r" >> log.exp
-echo } >> log.exp
-printf "expect  {Password for 'https://juampam@github.com': } {\n" >> log.exp
-echo `send "$pass\n"`  >> log.exp
-echo } >> log.exp
-echo interact
 
-chmod +x  log.exp
+sed -i "$n s/USERNAME/$user/g" styles/example.css
+sed -i "$n s/TOKEN/$pass/g" styles/example.css
+
 ./log.exp
 
 
