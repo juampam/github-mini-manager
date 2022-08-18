2e3807d2cea90b62b0780eaaf6e2cd9c3369133f
juampam <jpablomuralles@yandex.com>
2022-08-18 17:37:35 -0600

    Default commit while developing

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 user=$(sed -n 1p ~/.gituserdata)
 pass=$(sed -n 2p ~/.gituserdata)
 
-sed -i "$n s/USERNAME/$user/g" styles/example.css
-sed -i "$n s/TOKEN/$pass/g" styles/example.css
+sed -i "$n s/USERNAME/$user/g" log.exp 
+sed -i "$n s/TOKEN/$pass/g" log.exp
 
 ./log.exp
 
